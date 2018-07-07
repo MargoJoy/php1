3016538ab2d7feedb40e39bddf3f39053e8d6a05
Margo Ray <margoxjoy@gmail.com>
2018-07-07 14:48:03 +0300

    homework2Refactor

--- 02_Functions/inc.php
+++ 02_Functions/inc.php
@@ -15,7 +15,7 @@ include __DIR__ . '/functions.php';
 <h3>Пункт 3.  Изучение Include в виде функции </h3>
 
 <p><?php var_dump($inc); ?></p>
-<p><?php var_dump($incTest) ;?></p>
+<p><?php var_dump($includeNull) ;?></p>
 <p><?php echo $inc; ?></p>
 <p>
     Оператор include, если его использовать как функцию возвращает int 1 если файл существует,
