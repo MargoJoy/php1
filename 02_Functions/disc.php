e2a31c2c212435743e5948c2f585c68ca4212ee0
Margo Ray <margoxjoy@gmail.com>
2018-07-07 14:53:31 +0300

    homework2Refactor

--- 02_Functions/disc.php
+++ 02_Functions/disc.php
@@ -13,7 +13,6 @@ include __DIR__ . '/functions.php';
 <body>
 
 <h3>Пункт 2.  Корень квадратного уравнения </h3>
-
 <p>Дискриминант = <?php echo dis($a, $b, $c); ?> </p>
 <p><?php echo $res; ?> </p>
 <p><?php echo $x1; ?> </p>
