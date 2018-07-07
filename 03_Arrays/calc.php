3c250cc27d72995f55c64e0517a953abeaa98ed2
Margo Ray <margoxjoy@gmail.com>
2018-07-07 17:30:46 +0300

    homework3

--- 03_Arrays/calc.php
+++ 03_Arrays/calc.php
@@ -1,29 +1,67 @@
 <?php
-include __DIR__ . '/funcCalc.php';
+//===========Калькулятор===========
+//если были установлены переменные
+if (isset($_GET['x']) || isset($_GET['y']) || isset($_GET['arithmetic'])) {
+//то присваиваем их значение новым переменным для дальнейшей работы
+    $x = $_GET['x'];
+    $y = $_GET['y'];
+    $oper = $_GET['arithmetic'];
+//иначе присваиваем переменным значение null
+} else {
+    $x = null;
+    $y = null;
+    $oper = null;
+}
+
+//функция принимает 3 оператора (переменные в которых хранятся полученные значения)
+function calc($x, $y, $oper)
+{
+//если значения равны null, или одно значение равно null возвращается просьба заполнить паля
+    if ($x == null || $y == null || $oper == null) {
+        return null;
+//иначе начинаем проверять какие значения введены
+    } else {
+//проводит мат. операции, с поправкой / 0
+        if ($oper == '+') {
+            return  $x + $y;
+        } elseif ($oper == '-'){
+            return  $x - $y;
+        } elseif ($oper == '*'){
+            return  $x * $y;
+        } elseif ($oper == '/'){
+            if($x == 0 || $y == 0 ){
+                return null;
+            } else {
+                return  $x / $y;
+            }
+        }
+    }
+}
+//вывод получившегося значения
+$z = calc($x, $y, $oper);
+
 ?>
+<h3>Калькулятор</h3>
 
-<div class="calc">
-    <h3>Калькулятор</h3>
-
-    <form action="" method="get">
-        <label for="">
+<form action="/calc.php" method="get">
+    <label for="">
 <!--Вывод введенных значений-->
-            <input type="number" name="x" value="<?php echo $x; ?>">
-        </label>
-        <select name="arithmetic" id="">
-            <option value="+"> + </option>
-            <option value="-"> - </option>
-            <option value="*"> * </option>
-            <option value="/"> / </option>
-        </select>
-        <label for="">
+        <input type="number" name="x" value="<?php echo $x; ?>">
+    </label>
+    <select name="arithmetic" id="">
+        <option value="+"> + </option>
+        <option value="-"> - </option>
+        <option value="*"> * </option>
+        <option value="/"> / </option>
+    </select>
+    <label for="">
 <!--Вывод введенных значений-->
-            <input type="number" name="y" value="<?php echo $y; ?>">
-        </label>
-        <label for="">
-            <input type="submit" value="=">
-        </label>
+        <input type="number" name="y" value="<?php echo $y; ?>">
+    </label>
+    <label for="">
+        <input type="submit" value="=">
+    </label>
 <!--Вывод результата-->
-        <span> <?php echo $z; ?></span>
-    </form>
-</div>
+    <span> <?php echo $z; ?></span>
+</form>
+
--- 03_Arrays/gallery.php
+++ 03_Arrays/gallery.php
@@ -1,28 +1,15 @@
 <?php
 include __DIR__ . '/imgArray.php';
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Массивы. Циклы. Взаимодействие с пользователем.</title>
 
-</head>
-<body>
-<div class="gal">
-    <h3>Галерея</h3>
-<!--перебор массива по ключу-->
+<h3>Галерея</h3>
+
+<!--перебор массива по ключу, сам массив и переменная $image в файле -->
     <?php foreach ($image as $kay => $img) { ?>
 <!--в идентификатор ссылки передается ключ от имени каждой картинуи в массиве-->
-        <a href="image.php?id=<?php echo $kay; ?>">
+        <a href="/image.php?id=<?php echo $kay; ?>">
 <!--выводится все что есть в массиве в путь до изображения, по очереди, если по такому пути есть изображение,
 с таким именем, оно будет выведено-->
-            <img src="img/<?php echo $img; ?>" alt="">
+            <img width="600px" src="/img/<?php echo $img; ?>" alt="">
         </a>
     <?php } ?>
-</div>
-</body>
-</html>
