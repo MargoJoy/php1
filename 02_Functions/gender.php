876230d0684a51535a94dbd7e014b39d84ad82d1
Margo Ray <margoxjoy@gmail.com>
2018-07-08 19:43:38 +0300

    homework2Refactor

--- 02_Functions/gender.php
+++ 02_Functions/gender.php
@@ -6,16 +6,18 @@ function gender($name)
     $genderString = mb_substr($name, - 1);
 //переводит эту часть строки к нижнему регистру
     $genderName = mb_strtolower($genderString);
+    $women = 'Женский пол';
+    $men = 'Мужской пол';
 //если полученная часть строки совпадает с перечисленными то
     if ($genderName == 'а' || $genderName == 'я' || $genderName == 'ь') {
 //вернет значение
-        return 'Женский пол';
+        return $women;
 //если раньше не нашлось совпадений, то сравнить с другими
     } elseif ($genderName == 'л' || $genderName == 'р' || $genderName == 'й'||
         $genderName == 'н'|| $genderName == 'м' || $genderName == 'т' ||
         $genderName == 'с' || $genderName == 'в') {
 //если совпало то вернется
-        return 'Мужской пол';
+        return $men;
 //если снова не нашлось совпадений, то вернется null
     } else {
         return null;
