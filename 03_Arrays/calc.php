39e83bdaf1a792c21cad4b50a6bb16b318282039
Margo Ray <margoxjoy@gmail.com>
2018-07-08 14:56:01 +0300

    homework

--- 03_Arrays/calc.php
+++ 03_Arrays/calc.php
@@ -1,4 +1,5 @@
 <?php
+include __DIR__ . '/funCalc.php';
 //===========Калькулятор===========
 //если были установлены переменные
 if (isset($_GET['x']) || isset($_GET['y']) || isset($_GET['arithmetic'])) {
@@ -13,30 +14,7 @@ if (isset($_GET['x']) || isset($_GET['y']) || isset($_GET['arithmetic'])) {
     $oper = null;
 }
 
-//функция принимает 3 оператора (переменные в которых хранятся полученные значения)
-function calc($x, $y, $oper)
-{
-//если значения равны null, или одно значение равно null возвращается просьба заполнить паля
-    if ($x == null || $y == null || $oper == null) {
-        return null;
-//иначе начинаем проверять какие значения введены
-    } else {
-//проводит мат. операции, с поправкой / 0
-        if ($oper == '+') {
-            return  $x + $y;
-        } elseif ($oper == '-'){
-            return  $x - $y;
-        } elseif ($oper == '*'){
-            return  $x * $y;
-        } elseif ($oper == '/'){
-            if($x == 0 || $y == 0 ){
-                return null;
-            } else {
-                return  $x / $y;
-            }
-        }
-    }
-}
+
 //вывод получившегося значения
 $z = calc($x, $y, $oper);
 
