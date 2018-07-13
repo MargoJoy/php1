3fd94b8523d41b09cdf70c8a77bca9408050bad8
Margo Ray <margoxjoy@gmail.com>
2018-07-13 14:01:09 +0300

    homework4

---
+++ 04_Upload/upType.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'image/jpeg',
+    'image/png',
+    ];
--- 04_Upload/upload.php
+++ 04_Upload/upload.php
@@ -1,22 +1,18 @@
 <?php
+
+$type = include __DIR__ . '/upType.php';
+
 //если файл сушествует
 if (isset($_FILES['newImg'])) {
-//если в файле нет ошибок
-    if (0 == $_FILES['newImg']['error']) {
+//если в файле нет ошибок если последний последний элемент массива соответствует
+    if ((0 == $_FILES['newImg']['error']) && in_array($_FILES['newImg']['type'], $type)) {
 //имя полученное от пользователя
-        $name = basename($_FILES['newImg']['name']);
-//массив из имени разделенный точкой
-        $get_name = explode('.', $name);
-//последний элемент массива имени к верхнему регистру
-        $val = mb_strtoupper(end($get_name));
-//если последний последний элемент массива соответствует
-        if ($val == ('JPG' || 'JPEG' || 'PNG' )) {
+        $name = $_FILES['newImg']['name'];
 //загрузка в указанную директорию с именем полученным от пользователя
-            move_uploaded_file(
-                $_FILES['newImg']['tmp_name'],
-                __DIR__ . '/img/' . $name
-            );
-        }
+        move_uploaded_file(
+            $_FILES['newImg']['tmp_name'],
+            __DIR__ . '/img/' . $name
+        );
     }
 //перенаправляет на страницу галереи
     header('Location: /gallery.php');
