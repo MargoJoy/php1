ea9b0a65fa63796cf1e602ea5394bb096ad3517d
Margo Ray <margoxjoy@gmail.com>
2018-07-13 14:08:03 +0300

    homework5

--- 05_Session/upload.php
+++ 05_Session/upload.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 include __DIR__ . '/functions.php';
+$type = include __DIR__ . '/upType.php';
 
 //пользователь
 $userName = getCurrentUser();
@@ -15,18 +16,16 @@ $logPath = __DIR__ . '/log.txt';
 
 if (isset($_FILES['newImg'])) {
 //проверка на наличие файла и существование пользователя
-    if ((0 == $_FILES['newImg']['error']) && (isset($userName))) {
+    if ((0 == $_FILES['newImg']['error']) && in_array($_FILES['newImg']['type'], $type) && (isset($userName))) {
         $name = $_FILES['newImg']['name'];
-        $get_name = explode('.', $name);
-        $val = mb_strtoupper(end($get_name));
-        if ($val == ('JPG' || 'JPEG' || 'PNG' )) {
+
            move_uploaded_file(
                 $_FILES['newImg']['tmp_name'],
                 __DIR__ . '/img/' . $name
             );
 //запись в лог при загрузке файла
             file_put_contents($logPath, $log, FILE_APPEND);
-        }
+
     }
     header('Location: /index.php');
 }
